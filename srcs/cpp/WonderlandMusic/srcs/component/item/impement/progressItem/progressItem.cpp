2ea61f467427fe473ed4ccaae22d6a22176a2eb3
Acreate <1552397900@qq.com>
2026-09-15 10:23:24 +0800

    改进 ProgressItem

--- srcs/cpp/WonderlandMusic/srcs/component/item/impement/progressItem/progressItem.cpp
+++ srcs/cpp/WonderlandMusic/srcs/component/item/impement/progressItem/progressItem.cpp
@@ -3,85 +3,52 @@
 #include <QPainter>
 #include <QRect>
 
-#include <tools/calculateTools.h>
+#include <head/result_message_out.h>
 
-#include "../../../../head/result_message_out.h"
+ProgressItem::ProgressItem( ) : ProgressItem( 100 ) {
+}
+ProgressItem::ProgressItem( ProgressItemDouble var ) : var( var ) {
+}
 
-ProgressItem::ProgressItem( ) : ProgressItem( 0, CalculateTools::max< size_t >( ) ) {
-}
-ProgressItem::ProgressItem( const size_t &current_var ) : ProgressItem( current_var, CalculateTools::max< size_t >( ) ) {
-}
-ProgressItem::ProgressItem( const size_t &current_var, const size_t &max_var ) : maxVar( max_var ) {
-	if( setCurrentVar( current_var ) == false )
-		currenVar = 0;
-}
 ProgressItem::~ProgressItem( ) {
 }
-void ProgressItem::setMaxValue( const size_t &max_var ) {
-	maxVar = max_var;
-}
-size_t ProgressItem::getMaxValue( ) const {
-	return maxVar;
-}
-bool ProgressItem::calculateXPosVar( size_t &result_var, const int &x_pos, const bool &is_ceil ) const {
+bool ProgressItem::calculateXPosVar( ProgressItemDouble &result_var, const int &x_pos ) const {
 	auto &&geometry = ICoord::getGeometry( );
-	int comp = geometry.right( );
-	if( comp > x_pos ) // 溢出右侧
+	auto porgressRight = geometry.right( );
+	if( porgressRight > x_pos ) // 溢出右侧
 		return false;
-	comp = geometry.left( );
-	if( comp < x_pos ) // 溢出左侧
+	auto porgressLeft = geometry.left( );
+	if( porgressRight < x_pos ) // 溢出左侧
 		return false;
-	comp -= x_pos;
-	double width = geometry.width( );
-	double mod = width / comp;
-	comp = mod;
-	if( is_ceil ) {
-		width = comp;
-		bool result;
-		if( CalculateTools::equ( result, mod, width ) )
-			if( result == false )
-				comp += 1;
-	}
-	mod = comp;
-	mod = mod / 100;
-	result_var = mod * maxVar;
+	result_var = porgressRight - porgressLeft;
+	ProgressItemDouble offsetX = x_pos - porgressLeft;
+	result_var = result_var / offsetX;
 	return true;
 }
-bool ProgressItem::calculateVarXPos( int &result_x_pos, const size_t &var, const bool &is_ceil ) const {
-	auto &geo = getGeometry( );
-	result_x_pos = geo.left( );
-	if( var == 0 )
-		true;
-	double cmp = geo.width( ) * var;
-	size_t offsetX = maxVar / cmp;
-	result_x_pos = result_x_pos + offsetX;
-	if( is_ceil ) {
-		offsetX = cmp;
-		double o = offsetX;
-		bool result;
-		if( CalculateTools::equ( result, o, cmp ) )
-			if( result == false )
-				result_x_pos += 1;
-	}
+bool ProgressItem::calculateVarXPos( int &result_x_pos, const ProgressItemDouble &new_var ) const {
+	if( new_var < 0 || new_var > ( ( ProgressItemDouble ) 100 ) )
+		return false;
+	auto &&geometry = ICoord::getGeometry( );
+	result_x_pos = geometry.width( ) * new_var;
 	return true;
 }
-size_t ProgressItem::getCurrentVar( ) const {
-	return currenVar;
+ProgressItem::ProgressItemDouble ProgressItem::getCurrentVar( ) const {
+	return var;
 }
-bool ProgressItem::setCurrentVar( const size_t &new_var ) {
+
+bool ProgressItem::setCurrentVar( const ProgressItemDouble &new_var ) {
 	if( IItemDraw::isNull( ) )
 		return false;
-	if( new_var > maxVar )
+	if( new_var < 0 || new_var > ( ( ProgressItemDouble ) 100 ) )
 		return false;
-	int drawEndX;
-	if( calculateVarXPos( drawEndX, new_var ) == false )
-		return false;
-	currenVar = new_var;
+	var = new_var;
+	auto &&geometry = ICoord::getGeometry( );
+	auto width = geometry.width( ) * var;
 	auto drawImageBuff = IItemDraw::getDrawImageBuffPtr( );
 	QPainter painter;
 	painter.begin( drawImageBuff );
 	int height = drawImageBuff->height( );
-	painter.fillRect( 0, 0, drawEndX, height, Qt::GlobalColor::darkGreen );
+	painter.fillRect( 0, 0, width, height, Qt::GlobalColor::darkGreen );
 	painter.end( );
 	return true;
 }
--- srcs/cpp/WonderlandMusic/srcs/component/item/impement/progressItem/progressItem.h
+++ srcs/cpp/WonderlandMusic/srcs/component/item/impement/progressItem/progressItem.h
@@ -3,23 +3,20 @@
 #include "../../interface/iItemDraw/iItemDraw.h"
 
 class ProgressItem : public IItemDraw {
-private:
-	size_t maxVar;
-	size_t currenVar;
+public:
+	using ProgressItemDouble = long double;
 
 protected:
+	ProgressItemDouble var;
 
 public:
 	ProgressItem( );
-	ProgressItem( const size_t &current_var );
-	ProgressItem( const size_t &current_var, const size_t &max_var );
+	explicit ProgressItem( ProgressItemDouble var );
 	~ProgressItem( ) override;
-	virtual void setMaxValue( const size_t &max_var );
-	virtual size_t getMaxValue( ) const;
-	virtual bool calculateXPosVar( size_t &result_var, const int &x_pos, const bool &is_ceil = true ) const;
-	virtual bool calculateVarXPos( int &result_x_pos, const size_t &var, const bool &is_ceil = true ) const;
-	virtual size_t getCurrentVar( ) const;
-	virtual bool setCurrentVar( const size_t &new_var );
+	virtual bool calculateXPosVar( ProgressItemDouble &result_var, const int &x_pos ) const;
+	virtual bool calculateVarXPos( int &result_x_pos, const ProgressItemDouble &var ) const;
+	virtual ProgressItemDouble getCurrentVar( ) const;
+	virtual bool setCurrentVar( const ProgressItemDouble &new_var );
 	bool loadFileToDraw( const QString &load_image_file_path ) override;
 	bool loadStringToDraw( const QString &set_string_draw_to_buff ) override;
 	bool setDraw( const QImage &draw ) override;
