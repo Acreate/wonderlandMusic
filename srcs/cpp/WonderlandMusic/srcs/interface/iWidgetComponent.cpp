3c5b088d95bfaafcfec23c9a0dd22c8e997bc7d0
Acreate <1552397900@qq.com>
2026-09-21 17:03:09 +0800

    增加接口

---
+++ srcs/cpp/WonderlandMusic/srcs/interface/iWidgetComponent.cpp
@@ -0,0 +1,1 @@
+﻿#include "iWidgetComponent.h"
---
+++ srcs/cpp/WonderlandMusic/srcs/interface/iWidgetComponent.h
@@ -0,0 +1,28 @@
+﻿#ifndef IWIDGETCOMPONENT_H_H_HEAD__FILE__
+#define IWIDGETCOMPONENT_H_H_HEAD__FILE__
+#include <qobject.h>
+
+#include "../classTypeInfo/classTypeInfoVar.h"
+
+class QSize;
+class QWidget;
+class QPoint;
+class QRect;
+
+class IWidgetComponent : public virtual ClassTypeInfoVar {
+public:
+	virtual const QRect & getGeometry( ) const;
+	virtual const QPoint & getPoint( ) const;
+	virtual bool setGeometry( const QRect &new_geometry ) const;
+	virtual bool setPoint( const QPoint &new_point );
+	virtual QWidget * toWidget( );
+	virtual const QWidget * toWidget( ) const;
+	virtual const QSize & getSize( ) const;
+	virtual bool setSize( const QSize &new_size ) const;
+	virtual bool setShowStatus( const bool &show_status );
+	virtual bool getShowStatus( ) const;
+	virtual bool setParent( IWidgetComponent *parent );
+	virtual IWidgetComponent * getParent( ) const;
+};
+
+#endif // IWIDGETCOMPONENT_H_H_HEAD__FILE__
